7924318f13a31634513f9bb794b529a8f941ded4
구남경 <koonamkyung@gunamgyeong-ui-MacBookPro.local>
2022-05-31 16:56:48 +0900

    13305

---
+++ python/greedy/13305.py
@@ -0,0 +1,20 @@
+# 주유소
+N = int(input())
+
+km = list(map(int, input().split()))
+
+price = list(map(int, input().split()))
+
+res = 0
+
+P = price[0]
+
+for i in range(N - 1):
+    if price[i] < P:
+        P = price[i]
+    res += P * km[i]
+
+print(res)
+
+# 5 2 4 1
+#  2 3 1R
