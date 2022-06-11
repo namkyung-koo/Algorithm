f4c53791b1072b66701ba11891e4988ed276b0f6
구남경 <koonamkyung@gunamgyeong-ui-MacBookPro.local>
2022-06-12 01:41:49 +0900

    4963

--- python/bfs&dfs/4963.py
+++ python/bfs&dfs/4963.py
@@ -2,8 +2,6 @@
 from collections import deque
 import sys
 
-Is = 0
-
 while True:
     # w = 지도의 너비, h = 지도의 높이
     w, h = map(int, sys.stdin.readline().split())
@@ -15,28 +13,30 @@ while True:
     
     visited = [[False] * w for _ in range(h)]
 
+    Is = 0
+
     # 위에서부터 시계방향으로 체크
     dx = [0, 1, 1, 1, 0, -1, -1, -1]
-    dy = [1, 1, 0, -1, -1, -1, 0, 1]
+    dy = [-1, -1, 0, 1, 1, 1, 0, -1]
 
-
-    def bfs(col, row):
-        global Is
-        queue = deque([(col, row)])
+    def bfs(y, x):
+        queue = deque([(y, x)])
         while queue:
             cy, cx = queue.popleft()
             for i in range(8):
                 ny = cy + dy[i]
                 nx = cx + dx[i]
                 # 상하좌우, 대각선 체크
-                if 0 <= nx < h and 0 <= ny < w:
+                if 0 <= nx < w and 0 <= ny < h:
                     if visited[ny][nx] == False and graph[ny][nx] == 1:
-                        visited[ny][nx] == True
+                        visited[ny][nx] = True
                         queue.append([ny, nx])
 
     for i in range(h):
         for j in range(w):
-            if graph[i][j] == 1 and visited[i][j] == False:
+            if visited[i][j] == False and graph[i][j] == 1:
                 visited[i][j] = True
                 bfs(i, j)
+                Is += 1
+                
     print(Is)
