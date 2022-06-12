a10febdd105507384ffceeb80293260acd83509e
구남경 <koonamkyung@gunamgyeong-ui-MacBookPro.local>
2022-06-12 19:47:19 +0900

    16173

--- python/bfs&dfs/16173.py
+++ python/bfs&dfs/16173.py
@@ -12,21 +12,18 @@ def bfs(y, x):
     queue = deque([(y, x)])
     while queue:
         cy, cx = queue.popleft()
+        dy = [cy, cy + graph[cy][cx]]
+        dx = [cx + graph[cy][cx], cx]
         for i in range(2):
-            nx = graph[y][x + graph[cy][cx]]
-            ny = graph[y + graph[cy][cx]][x]
+            ny = dy[i]
+            nx = dx[i]
             if 0 <= nx < N and 0 <= ny < N:
                 if visited[ny][nx] == False and graph[ny][nx] < N:
                     visited[ny][nx] = True
                     queue.append([ny, nx])
-                elif graph[ny][nx] == -1:
-                    return True
-
+                    if graph[ny][nx] == -1:
+                        return True
 if bfs(0, 0):
     print("HaruHaru")
-else:
-    print("Hing")
-
-# 현재 위치 방문 표시
-# 해당 인덱스의 값을 받아와서 val에 대입
-# if val == -1 이면 True를 리턴
+    exit()
+print("Hing")
