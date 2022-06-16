f083a09d6ee13320a4548028208278c4848a8457
구남경 <koonamkyung@gunamgyeong-ui-MacBookPro.local>
2022-06-17 07:04:36 +0900

    4949

--- python/stack/4949.py
+++ python/stack/4949.py
@@ -2,10 +2,26 @@
 import sys
 
 while True:
-    str = sys.stdin.readline()
+    # 자동으로 붙는 개행문자를 rstrip() method로 제거
+    str = sys.stdin.readline().rstrip()
     
-    if str == ".":
+    if str == '.':
         break
 
     stack = []
+
+    for i in range(len(str)):
+        if str[i] == '(' or str[i] == '[':
+            stack.append(str[i])
+        elif stack and stack[-1] == '[' and str[i] == ']':
+            stack.pop()
+        elif stack and stack[-1] == '(' and str[i] == ')':
+            stack.pop()
+        elif str[i] == ')' or str[i] == ']':
+            stack.append(str[i])
+            break
     
+    if not stack:
+        print("yes")
+    else:
+        print("no")
